7d9d5a8f655bc42026d313c5bcb7d8b6b2823d77
xiaomiyao <chantseva123@gmail.com>
2025-04-10 11:58:02 +0200

    added memory monitoring

--- Modules/MemoryMonitor.cs
+++ Modules/MemoryMonitor.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management;
 
 namespace SystemUtilityApp.Modules
 {
-    internal class MemoryMonitor
+    public static class MemoryMonitor
     {
+        public static void Run()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Memory Monitor ===\n");
+
+            try
+            {
+                ulong totalMemory = 0;
+                ulong freeMemory = 0;
+
+                var searcher = new ManagementObjectSearcher("SELECT TotalVisibleMemorySize, FreePhysicalMemory FROM Win32_OperatingSystem");
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    totalMemory = Convert.ToUInt64(obj["TotalVisibleMemorySize"]);
+                    freeMemory = Convert.ToUInt64(obj["FreePhysicalMemory"]);
+                }
+
+                ulong usedMemory = totalMemory - freeMemory;
+
+                // Convert from KB to bytes
+                totalMemory *= 1024;
+                freeMemory *= 1024;
+                usedMemory *= 1024;
+
+                Console.WriteLine($"Total Memory:     {FormatBytes(totalMemory)}");
+                Console.WriteLine($"Used Memory:      {FormatBytes(usedMemory)}");
+                Console.WriteLine($"Available Memory: {FormatBytes(freeMemory)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to read memory info:");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("\nPress any key to return to the menu...");
+            Console.ReadKey();
+        }
+
+        private static string FormatBytes(ulong bytes)
+        {
+            double gb = bytes / (1024.0 * 1024 * 1024);
+            return $"{gb:0.00} GB";
+        }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ while (true)
     switch (input)
     {
         case "1": CpuMonitor.Run(); break;
-        //case "2": MemoryMonitor.Run(); break;
+        case "2": MemoryMonitor.Run(); break;
         //case "3": DiscMonitor.Run(); break;
         //case "4": ProcessManager.Run(); break;
         //case "5": FileManager.Run(); break;
